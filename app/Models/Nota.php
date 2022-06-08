0177b3a073fbba1f159487cbcfedac10ca945bb6
titicuevas <enrique.cuevas@iesdonana.org>
2022-06-08 11:07:28 +0200

    Modelos completos

--- app/Models/Nota.php
+++ app/Models/Nota.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Nota extends Model
 {
     use HasFactory;
+
+    public function alumno(){
+        return $this->belongsTo(Alumno::class);
+    }
+
+    public function ccee(){
+        return $this->belongsTo(Ccee::class);
+    }
 }
