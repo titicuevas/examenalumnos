a8e7ad0918400235e9a4dd19770453d5e31c76a0
titicuevas <enrique.cuevas@iesdonana.org>
2022-06-08 14:31:55 +0200

    Editado los criterios y arreglar el dashboard

--- resources/views/alumnos/criterios.blade.php
+++ resources/views/alumnos/criterios.blade.php
@@ -1,4 +1,69 @@
+<x-app-layout>
+    <x-slot name="header">
+        Criterios
+    </x-slot>
+    <div class="flex flex-col items-center mt-4">
+        <h1 class="mb-4 text-2xl font-semibold">Criterios</h1>
+        <div> Nombre del alumno: {{$alumno->nombre}}</div>
+        <div class="border border-gray-200 shadow">
 
+            <table>
+                <thead class="bg-gray-50">
+                    <tr>
+                        <th class="px-6 py-2 text-xs text-gray-500">
+                            Id
+                        </th>
+
+                        <th class="px-6 py-2 text-xs text-gray-500">
+                            Nombre
+                        </th>
+                        <th class="px-6 py-2 text-xs text-gray-500">
+                            Asignatura
+                        </th>
+                        <th class="px-6 py-2 text-xs text-gray-500">
+                            Nota
+                        </th>
+                    </tr>
+
+                </thead>
+                <tbody class="bg-white">
+                    @foreach ($alumno->notas as $nota)
+                        <tr class="whitespace-nowrap">
+                            <td class="px-6 py-4 text-sm text-blue-500 hover:underline">
+                                <a href="{{ route('alumnos.show', $alumno) }}">
+                                    {{ $alumno->id }}
+                                </a>
+                            </td>
+                            <td class="px-6 py-4">
+                                <div class="text-sm text-gray-900">
+                                    {{$nota->ccee->ce}}
+                                </div>
+                            </td>
+
+                            <td class="px-6 py-4">
+                                <div class="text-sm text-gray-900">
+                                    {{$nota->ccee->descripcion}}
+                                </div>
+                            </td>
+
+
+                            <td class="px-6 py-4">
+                                <div class="text-sm text-gray-900">
+
+                                    {{$nota->nota}}
+                                </div>
+                            </td>
+
+
+
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+        <a href="/alumnos" class="mt-4 text-blue-900 hover:underline">Volver a los alumnos</a>
+    </div>
+
+</x-app-layout>
 {{$alumno}}
 
 <br>
